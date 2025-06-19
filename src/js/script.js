1e580524f1c4360ed21362575d8835409a2d359b
yoyo331 <contact@maksim.fit>
2025-06-20 00:51:12 +0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const socket = io('http://localhost:5000');
+  const socket = io('http://localhost:3000');
 
   const $ = (id) => {
     return document.getElementById(id);
